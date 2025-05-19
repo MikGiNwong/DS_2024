eaf6256b9a7d42695193327c83b32e3e4b2579c9
MikGiNwong <0614kht@naver.com>
2025-05-19 14:54:00 +0900

    .

--- src/main/BinaryTreeTravel.java
+++ src/main/BinaryTreeTravel.java
@@ -16,7 +16,7 @@ public class BinaryTreeTravel<Key extends Comparable<Key>> {
         return list;
     }
 
-    private void preOrder(Node<Key> node, List<Key> list) {
+    private void preOrder(Node<Key> node, List<Key> list) { // 전위순회
         if (node == null) return;
         list.add(node.getItem());
         preOrder(node.getLeft(), list);
@@ -29,8 +29,12 @@ public class BinaryTreeTravel<Key extends Comparable<Key>> {
         return list;
     }
 
-    private void inOrder(Node<Key> node, List<Key> list) {
+    private void inOrder(Node<Key> node, List<Key> list) { // 중위순회
         // TODO preOrder를 참고하여 해당 메소드를 완성시키시오.
+        if(node == null) return;
+        inOrder(node.getLeft(), list);
+        list.add(node.getItem());
+        inOrder(node.getRight(), list);
     }
 
     public List<Key> postOrder() {
@@ -39,8 +43,12 @@ public class BinaryTreeTravel<Key extends Comparable<Key>> {
         return list;
     }
 
-    private void postOrder(Node<Key> node, List<Key> list) {
+    private void postOrder(Node<Key> node, List<Key> list) { // 후위순회
         // TODO preOrder를 참고하여 해당 메소드를 완성시키시오.
+        if(node == null) return;
+        postOrder(node.getLeft(), list);
+        postOrder(node.getRight(), list);
+        list.add(node.getItem());
     }
 
     public List<Key> levelOrder() {
@@ -51,5 +59,18 @@ public class BinaryTreeTravel<Key extends Comparable<Key>> {
 
     private void levelOrder(Node<Key> node, List<Key> list) {
         // TODO java.util.Queue 와 자료구조 PDF를 참고하여 해당 메소드를 완성시키시오.
+        Queue<Node<Key>> queue = new LinkedList<>();
+        queue.add(node);
+        while(!queue.isEmpty()){
+            Node<Key> n = queue.poll();
+            if(n == null) break;
+            if(n.getLeft() != null){
+                queue.add(n.getLeft());
+            }
+            if(n.getRight() != null){
+                queue.add(n.getRight());
+            }
+            list.add(n.getItem());
+        }
     }
 }
